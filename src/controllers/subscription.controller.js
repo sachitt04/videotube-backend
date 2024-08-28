d7c181a435196d21fe0f1f3a6f4943ccf31fe18d
sachit <sachitforsocials@gmail.com>
2024-08-28 12:25:24 +0530

    changes in suscription controllers

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,11 +9,62 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    if(!channelId || !isValidObjectId(channelId)){
+        throw new ApiError(400,"invalid channel id")
+    }
+
+    // check for already subscribed or not
+    const existingSubscriptionStatus = await Subscription.findOne({
+        subscriber:req.user?._id,
+        channelId: channelId
+    })
+
+    if(existingSubscriptionStatus){
+        // if subscribed then we have to remove subscribe
+
+        const unsubscribe = await Subscription.findByIdAndDelete(existingSubscriptionStatus);
+        if(!unsubscribe){
+            throw new ApiError(500,"error while unsubscribing")
+        }
+    } else{
+        // if not subscribed then add subscription
+        const subscribe = await Subscription.create({
+            subscriber: req.user?._id,
+            channel :channelId
+    })
+
+    if(!subscribe){
+        throw new ApiError(500,"error while subscribing")
+     }
+    }
+
+    // displying the subscribers count
+    const subscribers = await Subscription.find({
+        channel:channelId,
+    }).countDocuments()
+    console.log(subscribers)    
+
+    return res
+    .staus(200)
+    .json(
+        new ApiResponse(
+            200,
+            {
+
+            subscribers:subscribers,
+            isSubscribed:!existingSubscriptionStatus
+            // subscription toggled
+            }
+        )
+    )
 })
+
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+
+    
 })
 
 // controller to return channel list to which user has subscribed
